9bb5a9fed4f240928c46231ca44df463a63f79b3
François Monniot <francoismonniot@gmail.com>
2023-02-11 13:45:21 -0800

    Make when impl slightly more robust
    support for T & Mock, better error reporting, scalafmt

--- scala3mock/src/main/scala/macros/WhenImpl.scala
+++ scala3mock/src/main/scala/macros/WhenImpl.scala
@@ -7,6 +7,10 @@ private[macros] object WhenImpl:
 
   import scala.quoted.*
 
+  // We disable formatting for the apply & impl functions so that we can save line height.
+  // Without disable, appy12 would take around 36 lines vs the current one. impl is even worse.
+  // format: off
+
   inline def apply[R](inline f: () => R): MockFunction0[R] =
     ${impl('f)}
 
@@ -86,6 +90,7 @@ private[macros] object WhenImpl:
       Type[R], Quotes): Expr[MockFunction10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R]] =
     '{${createMockFunction(f)}.asInstanceOf[MockFunction10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R]]}
 
+  // format: on
 
   def createMockFunction(expr: Expr[Any])(using Quotes): Expr[MockFunction] =
     import quotes.reflect.*
@@ -93,33 +98,56 @@ private[macros] object WhenImpl:
     val (obj, name) = transcribeTree(expr.asTerm)
     println(s"access mock with the name $name")
 
-    '{ 
-      if ${obj.asExprOf[Any]}.isInstanceOf[Mock]
-      then ${obj.asExprOf[Any]}.asInstanceOf[Mock].accessMockFunction(${Expr(name)})
+    '{
+      if ${ obj.asExprOf[Any] }.isInstanceOf[Mock]
+      then
+        ${ obj.asExprOf[Any] }
+          .asInstanceOf[Mock]
+          .accessMockFunction(${ Expr(name) })
       else throw Mock.ReceiverIsNotAMock
     }
 
-
-  def transcribeTree(using quotes: Quotes)(tree: quotes.reflect.Term): (quotes.reflect.Term, String) =
+  def transcribeTree(using quotes: Quotes)(
+      tree: quotes.reflect.Term
+  ): (quotes.reflect.Term, String) =
     import quotes.reflect.*
 
     tree match
-      case Inlined(_, _, body) => transcribeTree(body)
-      case s@Select(qualifier, name) =>
-        val qualTpe = qualifier.tpe
-        val clsMethods = qualifier.tpe.classSymbol.map(_.methodMembers)
+      case Inlined(_, _, body)         => transcribeTree(body)
+      case s @ Select(qualifier, name) =>
+        // We don't simply use the qualTpe.classSymbol accessor because we might
+        // have multiple known type mixed in. At the moment we only support excluding
+        // the Mock trait from one other base class. The filtering here needs to change
+        // if (or when) the when macro support union, intersection or with mixin.
+        //
+        // dev note: TLDR the filtering is only useful when using `MockImpl.apply` or
+        // `MockImpl.debug` because those returns `T & Mock`.
+        val objectSym = Symbol.requiredClass("java.lang.Object")
+        val anySym = Symbol.requiredClass("scala.Any")
+        val matchableSym = Symbol.requiredClass("scala.Matchable")
+        val mockSym = Symbol.requiredClass("context.Mock")
+        val classSymbol = qualifier.tpe.baseClasses
+          .filterNot(sym =>
+            sym == objectSym || sym == anySym || sym == matchableSym || sym == mockSym
+          )
+          .headOption
 
         val n = s.signature match
           case None =>
             // No signature means a nullary function. The mock name will always be the
             // same as the function name (no overload possible with nullary)
-            name 
-          
+            name
+
           case Some(signature) =>
-            clsMethods match
-              case None => ??? // TODO Is it possible ?
+            classSymbol.map(_.methodMembers) match
+              case None =>
+                report.errorAndAbort(
+                  "The when parameter is composed of more than one type, which isn't supported at the moment."
+                )
+
               case Some(methods) =>
-                val overload = utils.sortSymbolsViaSignature(methods.filter(_.name == name))
+                val overload =
+                  utils.sortSymbolsViaSignature(methods.filter(_.name == name))
 
                 // If there are no overload, let's use the method name as the mock key. Otherwise
                 // append the index of the overload. Using the same sort here and in the mock
@@ -130,23 +158,35 @@ private[macros] object WhenImpl:
 
                   s"${name}-$idx"
                 }
-        
+
         qualifier -> n
 
       case Block(stats, _) =>
-        //println(stats.map(_.getClass))
+        // println(stats.map(_.getClass))
 
         stats.collectFirst {
           case term: Term => transcribeTree(term)
-          case t: DefDef =>
-            //println(s"DefDef.rhs = ${t.rhs}")
-            t.rhs.fold(throw new MatchError("DefDef without a body found. Can't find structure."))(transcribeTree)
+          case t: DefDef  =>
+            // println(s"DefDef.rhs = ${t.rhs}")
+
+            t.rhs match
+              case None =>
+                report.errorAndAbort(
+                  "Method definition doesn't have a body to investigate. Please report your use case to the maintainers."
+                )
+              case Some(term) => transcribeTree(term)
         }.get
 
       case Apply(fun, _) => transcribeTree(fun)
 
       case TypeApply(fun, _) => transcribeTree(fun)
-      case Ident(fun) => throw new MatchError(s"please declare '$fun' as MockFunctionXX or StubFunctionXX (e.g val $fun: MockFunction1[X, R] = ... if it has 1 parameter)")
-      case _ => throw new MatchError(
-        s"ScalaMock: Unrecognised structure: ${tree.show(using Printer.TreeStructure)}." +
-          "Please open a ticket at https://github.com/fmonniot/scala3mock/issues")
+      case Ident(fun)        =>
+        // TODO That's probably not a valid advice anymore. Write a test case and update the error message accordingly.
+        report.errorAndAbort(
+          s"please declare '$fun' as MockFunctionXX or StubFunctionXX (e.g val $fun: MockFunction1[X, R] = ... if it has 1 parameter)"
+        )
+      case _ =>
+        report.errorAndAbort(
+          s"ScalaMock: Unrecognised structure: ${tree.show(using Printer.TreeStructure)}." +
+            "Please open a ticket at https://github.com/fmonniot/scala3mock/issues"
+        )
