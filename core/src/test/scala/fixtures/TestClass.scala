7eb62f731673dca7e7a6b740a89ecd341819aa66
François Monniot <francoismonniot@gmail.com>
2023-04-06 23:19:26 -0700

    add todo

--- core/src/test/scala/fixtures/TestClass.scala
+++ core/src/test/scala/fixtures/TestClass.scala
@@ -1,5 +1,8 @@
 package fixtures
 
+// TODO Add a simple parameter to verify we can mock class with
+// value parameter but no type parameter. I think I screwed up
+// that case when adding type parameters support.
 class TestClass {
   private def pm(x: Int) = x
   def m(x: Int, y: String) = (x, y)
