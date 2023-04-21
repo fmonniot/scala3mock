19a4d1263f04837a5d23ee68facccf758db317aa
François Monniot <francoismonniot@gmail.com>
2023-04-21 07:16:03 -0700

    fix two TODOs

--- core/src/main/scala/eu/monniot/scala3mock/handlers/UnorderedHandlers.scala
+++ core/src/main/scala/eu/monniot/scala3mock/handlers/UnorderedHandlers.scala
@@ -8,25 +8,32 @@ class UnorderedHandlers(logging: Boolean = false) extends Handlers:
   def handle(call: Call): Option[Any] = this.synchronized {
     if logging then println(s"handling unordered call $call")
 
-    // TODO Rewrite
-    returning {
-      for handler <- handlers do
-        val r = handler.handle(call)
-        if r.isDefined then throwReturn(r)
-      None
-    }
+    findFirstOpt(handlers, _.handle(call))
   }
 
   def verify(call: Call): Boolean = this.synchronized {
     if logging then println(s"verifying unordered call $call")
 
-    // TODO rewrite
-    returning {
-      for handler <- handlers do if handler.verify(call) then throwReturn(true)
-      false
-    }
+    handlers.find(_.verify(call)).isDefined
   }
 
   def reset(): Unit = handlers.foreach(_.reset())
 
   protected val prefix = "inAnyOrder"
+
+  /** Finds the first element of `iter` satisfying a predicate, if any.
+    *
+    *  @param iter    the collection of elements
+    *  @param p       the predicate used to test elements.
+    *  @return        an option value containing the first element in the $coll
+    *                 that satisfies `p.isDefined`, or `None` if none exists.
+    */
+  private def findFirstOpt[A, B](iter: IterableOnce[A], p: A => Option[B]): Option[B] = {
+    val it = iter.iterator
+    while (it.hasNext) {
+      val a = it.next()
+      val mayB = p(a)
+      if (mayB.isDefined) return mayB
+    }
+    None
+  }
