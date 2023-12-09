c01ebd5369e40b620ebadd6c8c15927b5bef4e7d
François Monniot <francoismonniot@gmail.com>
2023-12-09 08:10:37 -0800

    rename to ScalaMocks as it's the entry point for the library

--- core/src/main/scala/eu/monniot/scala3mock/ScalaMocks.scala
+++ core/src/main/scala/eu/monniot/scala3mock/ScalaMocks.scala
@@ -1,11 +1,11 @@
 package eu.monniot.scala3mock
 
-object Mocks extends Mocks
+object ScalaMocks extends ScalaMocks
 
 /** Helper trait that provide access to all components (mandatory or optional)
   * used by the library to build mocks.
   */
-trait Mocks
+trait ScalaMocks
     extends functions.MockFunctions
     with macros.Mocks
     with matchers.Matchers:
