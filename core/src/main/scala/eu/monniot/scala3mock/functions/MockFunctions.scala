d6ba914acff98a3a3758f50f875495236d46e05c
nikitaglushchenko <nikita.glushchenko@spendit.de>
2024-07-31 11:31:55 +0200

    apply scalafmt

--- core/src/main/scala/eu/monniot/scala3mock/functions/MockFunctions.scala
+++ core/src/main/scala/eu/monniot/scala3mock/functions/MockFunctions.scala
@@ -19,12 +19,26 @@ trait MockFunctions {
     new MockFunction5[T1, T2, T3, T4, T5, R](ctx, "MockFunction5")
   def mockFunction[T1, T2, T3, T4, T5, T6, R: Default](using ctx: MockContext) =
     new MockFunction6[T1, T2, T3, T4, T5, T6, R](ctx, "MockFunction6")
-  def mockFunction[T1, T2, T3, T4, T5, T6, T7, R: Default](using ctx: MockContext) =
+  def mockFunction[T1, T2, T3, T4, T5, T6, T7, R: Default](using
+      ctx: MockContext
+  ) =
     new MockFunction7[T1, T2, T3, T4, T5, T6, T7, R](ctx, "MockFunction7")
-  def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, R: Default](using ctx: MockContext) =
+  def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, R: Default](using
+      ctx: MockContext
+  ) =
     new MockFunction8[T1, T2, T3, T4, T5, T6, T7, T8, R](ctx, "MockFunction8")
-  def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, T9, R: Default](using ctx: MockContext) =
-    new MockFunction9[T1, T2, T3, T4, T5, T6, T7, T8, T9, R](ctx, "MockFunction9")
-  def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R: Default](using ctx: MockContext) =
-    new MockFunction10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R](ctx, "MockFunction10")
+  def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, T9, R: Default](using
+      ctx: MockContext
+  ) =
+    new MockFunction9[T1, T2, T3, T4, T5, T6, T7, T8, T9, R](
+      ctx,
+      "MockFunction9"
+    )
+  def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R: Default](using
+      ctx: MockContext
+  ) =
+    new MockFunction10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R](
+      ctx,
+      "MockFunction10"
+    )
 }
